0aeff7627b2dfd69bf15aaa536bb5cacc81e24cb
Tim Schulte <tschult0@chicagobooth.edu>
2017-08-02 01:25:31 +0000

    msm_associations10

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -19,4 +19,8 @@ class Actor < ApplicationRecord
 #  - bio: no rules
 #  - image_url: no rules
 
+has_many(:movies, :class_name => "Movie", :foreign_key => "actor_id")
+
+has_many(:characters, :class_name => "Character", :foreign_key => "actor_id")
+
 end
